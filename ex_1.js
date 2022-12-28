28ec07d455d5dbfe7ee71055c064b356c5743f49
Sackrapee <s6206021611052@email.kmutnb.ac.th>
2022-12-28 11:16:14 +0700

    create ex_1.js

---
+++ ex_1.js
@@ -0,0 +1,5 @@
+const findPerimeter = (length, width) => console.log((length, width)*2) ;
+
+findPerimeter(6,7);
+findPerimeter(20,10);
+findPerimeter(2,9);
---
+++ ex_2.js
@@ -0,0 +1,8 @@
+let num1 ="";
+function equation(num1){
+    result = eval(num1);
+    return console.log(result);
+}
+equation("1+1");
+equation("7*4-2");
+equation("1+1+1+1+1");
