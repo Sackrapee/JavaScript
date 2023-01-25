fde63d8811d972eeca5eb4bf23f3176adacea9cd
Sackrapee <s6206021611052@email.kmutnb.ac.th>
2023-01-25 11:29:40 +0700

    updateMID

---
+++ TimeConversion_Mid.js
@@ -0,0 +1,9 @@
+/*
+digitalClock(5025) = "01:25:45"
+// 5025 seconds is i hour, 23 mins, 45 secs.
+digitalClock(61201) = "17:00:01"
+// No AM/PM. 24h format.
+digitalClock(87000) = "00:10:00"
+// It's 00:10 next day.
+*/
+
